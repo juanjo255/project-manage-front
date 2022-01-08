06a9040985f3bcca4b5cc4183991e75b3cbf5946
juanjo255 <piconcossio@gmail.com>
2022-01-07 20:13:22 -0500

    logo nuevo y dialog en login

--- src/pages/auth/login.jsx
+++ src/pages/auth/login.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import useFormData from "hooks/useFormData";
 import { useMutation } from "@apollo/client";
@@ -7,13 +7,16 @@ import { useAuth } from "context/authContext";
 import { useNavigate } from "react-router-dom";
 import ReactLoading from "react-loading";
 import wave from "media/wave1.jpg";
-import logo from "media/EAFITlogo.png";
+import logo from "media/beakerLogo.png";
 import { toast } from "react-toastify";
+import { Dialog, DialogTitle, DialogContent, List, ListItem} from "@mui/material";
+
 
 const Login = () => {
   const navigate = useNavigate();
   const { setToken } = useAuth();
   const { form, formData, updateFormData } = useFormData();
+  const [open, setOpen] = useState(false);
 
   const [
     login,
@@ -41,35 +44,38 @@ const Login = () => {
 
   return (
     <div className="flex overflow-x-hidden ">
-      <img src={wave} className=" hidden lg:block  h-screen" alt="" />
-      <div class="absolute w-screen h-screen  justify-start items-center ">
+      <img src={wave} className=" hidden lg:block h-screen" alt="" />
+      <div className="absolute flex flex-col justify-center w-screen h-screen ">
         <form
           className="flex flex-col justify-center items-center "
           onSubmit={submitForm}
           onChange={updateFormData}
           ref={form}
         >
-          <img src={logo} alt="logoEAFIT" />
-          <h2 class="my-8 font-display font-bold text-3xl text-center">
-            SIGN IN
-          </h2>
+          <img src={logo} alt="logo" className="h-20" />
+          <div className="flex" onClick={()=>{setOpen(true)}}>
+            <h2 className="my-8 font-display font-bold text-3xl text-center">
+              RESEARCH PROJECTS 
+            </h2>
+            <i className="fas fa-question-circle hover:text-blue-700"/>
+          </div>
           <div className="relative">
             <i className="fa fa-user absolute text-blue-800 text-xl" />
             <input
               name="Email"
               type="text"
               placeholder="email"
-              class="pl-8 border-b-2 font-display focus:outline-none focus:border-blue-800 transition-all duration-500 capitalize text-lg"
+              className="pl-8 border-b-2 font-display focus:outline-none focus:border-blue-800 transition-all duration-500 capitalize text-lg"
               required={true}
             />
           </div>
-          <div class="relative mt-8">
-            <i class="fa fa-lock absolute text-blue-800 text-xl"></i>
+          <div className="relative mt-8">
+            <i className="fa fa-lock absolute text-blue-800 text-xl"></i>
             <input
               name="Password"
               type="password"
               placeholder="password"
-              class="pl-8 border-b-2 font-display focus:outline-none focus:border-blue-800 transition-all duration-500 capitalize text-lg"
+              className="pl-8 border-b-2 font-display focus:outline-none focus:border-blue-800 transition-all duration-500 capitalize text-lg"
               required={true}
             />
           </div>
@@ -92,6 +98,24 @@ const Login = () => {
           </Link>
         </form>
       </div>
+      <Dialog open={open} onClose={()=>(setOpen(false))}>
+        <DialogTitle sx={{fontWeight:800}} >Research Projects</DialogTitle>
+        <DialogContent className=" font-semibold">On this page, students can enroll in research projects of 
+          their interest, so here we find 3 Roles:
+        </DialogContent>
+        <List >
+          <ListItem className="dialog">
+            <span className="font-bold">Administrator:</span> can see and modify registered projects and users.
+          </ListItem>
+          <ListItem className="dialog">
+            <span className="font-bold">Leader:</span> can see and modify its projects and inscriptions to its projects.
+          </ListItem>
+          <ListItem className="dialog">
+            <span className="font-bold">Student: </span> only can see and enroll into active projects.
+          </ListItem>
+        </List>
+
+      </Dialog>
     </div>
   );
 };
